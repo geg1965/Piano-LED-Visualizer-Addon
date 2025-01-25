97197e2a3d17fd6e262383c1967b0b902d3b3dd0
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-01-25 18:03:34 +0100

    Update LED-Piano.php
    Use awk to only get the first IP

--- LED-Piano.php
+++ LED-Piano.php
@@ -4,7 +4,7 @@ function Redirect($url, $permanent = false)
   header('Location: ' . $url, true, $permanent ? 301 : 302);
   exit();
 }
-$ip = gethostbyname(trim(`hostname --all-ip-addresses`));
+$ip = gethostbyname(trim(`hostname --all-ip-addresses| awk '{print $1}'`));
 $para = $_GET["para"];
 $url="http://".$ip.$para;
 Redirect(($url), false);
