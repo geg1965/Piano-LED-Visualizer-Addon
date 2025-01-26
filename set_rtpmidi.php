0b869edaa6105a76376a5fd77ced8478e90b7252
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-01-26 18:42:08 +0100

    Update set_rtpmidi.php

--- set_rtpmidi.php
+++ set_rtpmidi.php
@@ -5,8 +5,16 @@ $ip3 = $_GET["ip3"];
 $ip4 = $_GET["ip4"];
 $port = $_GET["port"];
 $name = $_GET["name"];
-shell_exec("cp ./cfgs/default.ini /etc/rtpmidid/");
-shell_exec("echo [connect_to] >> /etc/rtpmidid/default.ini");
+$replace = $_GET["replace"];
+$minimal = $_GET["minimal"];
+if (isset($replace)) {
+  if (isset($minimal)) {
+    shell_exec("cp ./cfgs/minimal.ini /etc/rtpmidid/default.ini");
+  } else {
+    shell_exec("cp ./cfgs/default.ini /etc/rtpmidid/");
+  }
+}
+shell_exec("echo '\n[connect_to]' >> /etc/rtpmidid/default.ini");
 shell_exec("echo hostname=$ip1.$ip2.$ip3.$ip4 >> /etc/rtpmidid/default.ini");
 shell_exec("echo port=$port >> /etc/rtpmidid/default.ini");
 shell_exec("echo name=$name >> /etc/rtpmidid/default.ini");
