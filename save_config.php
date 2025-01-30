a71bdda730cc1cc89016e43ce7adff4a4997d4eb
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-01-30 16:38:59 +0100

    Update save_config.php

--- save_config.php
+++ save_config.php
@@ -1,10 +1,18 @@
-<?php
-$config = $_GET["config"];
-shell_exec("cp ../../config/settings.xml ./cfgs/settings.xml.$config");
-shell_exec("cp ../../config/sequences.xml ./cfgs/sequences.xml.$config");
-?>
-<script>
-var txt = "Preset <?php echo $config; ?> was saved!";
-alert(txt);
-window.location.href = 'presets.php';
-</script>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PLV save configuration</title>
+  </head>
+<body>
+  <?php
+    $config = $_GET["config"];
+    shell_exec("cp ../../config/settings.xml ./cfgs/settings.xml.$config");
+    shell_exec("cp ../../config/sequences.xml ./cfgs/sequences.xml.$config");
+  ?>
+  <script>
+    var txt = "Preset <?php echo $config; ?> wurde gespeichert!";
+    alert(txt);
+    window.location.href = 'presets.php';
+  </script>
+</body>
+</html>
