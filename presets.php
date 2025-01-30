ef508e48ad17b4759cde640ac0d73492700f1af1
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-01-30 16:11:35 +0100

    Update presets.php

--- presets.php
+++ presets.php
@@ -3,60 +3,92 @@
   <head>
     <title>PLV Presets</title>
     <style>
-    body {
-      background-image: url('/imgs/banner.png');
-      background-repeat: no-repeat;
-      background-position: center -3px;
+      @media (min-width: 861px) {
+        img {
+          display: flex;
+        }
+      }
+      @media (max-width: 860px) {
+        img {
+          display: none;
+        }
+      }
+    .banner {
+      position: absolute;
+      top: 0px;
+      left: calc(50% - 200px);
+      justify-content: center;
     }
-  </style>
+    select {
+      position: absolute;
+      top: 0px;
+      left: 5px;
+      width: 110px;
+      font-size: 12px;
+      border-radius: 10px;
+      padding: 6px 4px;
+      border: 1px solid #d7d7d7;
+      background: #fff;
+      background: rgba(0,0,0,0) url(./imgs/favicon-16x16.png) no-repeat scroll 80px center/18px auto;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      color: #000000;
+    }
+    </style>
   </head>
   <body>
-    <select size="1" name="jumpit" onchange="document.location.href=this.value">
-      <option selected value="">Load Preset</option>
-      <option value="./load_config.php?config=01">Load 01</option>
-      <option value="./load_config.php?config=02">Load 02</option>
-      <option value="./load_config.php?config=03">Load 03</option>
-      <option value="./load_config.php?config=04">Load 04</option>
-      <option value="./load_config.php?config=05">Load 05</option>
-      <option value="./load_config.php?config=06">Load 06</option>
-      <option value="./load_config.php?config=07">Load 07</option>
-      <option value="./load_config.php?config=08">Load 08</option>
-      <option value="./load_config.php?config=09">Load 09</option>
-      <option value="./load_config.php?config=10">Load 10</option>
-      <option value="./load_config.php?config=11">Load 11</option>
-      <option value="./load_config.php?config=12">Load 12</option>
-      <option value="./load_config.php?config=13">Load 13</option>
-      <option value="./load_config.php?config=14">Load 14</option>
-      <option value="./load_config.php?config=15">Load 15</option>
-      <option value="./load_config.php?config=16">Load 16</option>
-      <option value="./load_config.php?config=17">Load 17</option>
-      <option value="./load_config.php?config=18">Load 18</option>
-      <option value="./load_config.php?config=19">Load 19</option>
-      <option value="./load_config.php?config=20">Load 20</option>
+    <select size="1" left="200px" name="load" style="color: white" onchange="document.location.href=this.value">
+      <optgroup label="Load Preset:" style="color: black; font-family: 'Times New Roman', Times, serif;">
+      <option selected disabled hidden value="">Load Preset</option>
+      <option value="./load_config.php?config=01">Preset 01</option>
+      <option value="./load_config.php?config=02">Preset 02</option>
+      <option value="./load_config.php?config=03">Preset 03</option>
+      <option value="./load_config.php?config=04">Preset 04</option>
+      <option value="./load_config.php?config=05">Preset 05</option>
+      <option value="./load_config.php?config=06">Preset 06</option>
+      <option value="./load_config.php?config=07">Preset 07</option>
+      <option value="./load_config.php?config=08">Preset 08</option>
+      <option value="./load_config.php?config=09">Preset 09</option>
+      <option value="./load_config.php?config=10">Preset 10</option>
+      <option value="./load_config.php?config=11">Preset 11</option>
+      <option value="./load_config.php?config=12">Preset 12</option>
+      <option value="./load_config.php?config=13">Preset 13</option>
+      <option value="./load_config.php?config=14">Preset 14</option>
+      <option value="./load_config.php?config=15">Preset 15</option>
+      <option value="./load_config.php?config=16">Preset 16</option>
+      <option value="./load_config.php?config=17">Preset 17</option>
+      <option value="./load_config.php?config=18">Preset 18</option>
+      <option value="./load_config.php?config=19">Preset 19</option>
+      </optgroup>
     </select>
-
-    <select size="1" name="jumpit" onchange="document.location.href=this.value">
-      <option selected value="">Save Preset</option>
-      <option value="./save_config.php?config=01">Save 01</option>
-      <option value="./save_config.php?config=02">Save 02</option>
-      <option value="./save_config.php?config=03">Save 03</option>
-      <option value="./save_config.php?config=04">Save 04</option>
-      <option value="./save_config.php?config=05">Save 05</option>
-      <option value="./save_config.php?config=06">Save 06</option>
-      <option value="./save_config.php?config=07">Save 07</option>
-      <option value="./save_config.php?config=08">Save 08</option>
-      <option value="./save_config.php?config=09">Save 09</option>
-      <option value="./save_config.php?config=10">Save 10</option>
-      <option value="./save_config.php?config=11">Save 11</option>
-      <option value="./save_config.php?config=12">Save 12</option>
-      <option value="./save_config.php?config=13">Save 13</option>
-      <option value="./save_config.php?config=14">Save 14</option>
-      <option value="./save_config.php?config=15">Save 15</option>
-      <option value="./save_config.php?config=16">Save 16</option>
-      <option value="./save_config.php?config=17">Save 17</option>
-      <option value="./save_config.php?config=18">Save 18</option>
-      <option value="./save_config.php?config=19">Save 19</option>
-      <option value="./save_config.php?config=20">Save 20</option>
+&nbsp;
+    <select size="1" name="save" style="color: white; left: 125px;" onchange="document.location.href=this.value">
+      <optgroup label="Save Preset" style="color: black; font-family: 'Times New Roman', Times, serif;">
+      <option selected disabled hidden value="">Save Preset</option>
+      <option value="./save_config.php?config=01">Preset 01</option>
+      <option value="./save_config.php?config=02">Preset 02</option>
+      <option value="./save_config.php?config=03">Preset 03</option>
+      <option value="./save_config.php?config=04">Preset 04</option>
+      <option value="./save_config.php?config=05">Preset 05</option>
+      <option value="./save_config.php?config=06">Preset 06</option>
+      <option value="./save_config.php?config=07">Preset 07</option>
+      <option value="./save_config.php?config=08">Preset 08</option>
+      <option value="./save_config.php?config=09">Preset 09</option>
+      <option value="./save_config.php?config=10">Preset 10</option>
+      <option value="./save_config.php?config=11">Preset 11</option>
+      <option value="./save_config.php?config=12">Preset 12</option>
+      <option value="./save_config.php?config=13">Preset 13</option>
+      <option value="./save_config.php?config=14">Preset 14</option>
+      <option value="./save_config.php?config=15">Preset 15</option>
+      <option value="./save_config.php?config=16">Preset 16</option>
+      <option value="./save_config.php?config=17">Preset 17</option>
+      <option value="./save_config.php?config=18">Preset 18</option>
+      <option value="./save_config.php?config=19">Preset 19</option>
+      </optgroup>
     </select>
+    <div class="banner">
+      <img src="./imgs/banner.png" height="25" width="400">
+    </div>
   </body>
 </html>
