72d4d4cdf6d45e898054f479837196cdccf73c37
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-01-30 16:32:03 +0100

    Update load_config.php

--- load_config.php
+++ load_config.php
@@ -10,14 +10,3 @@ var txt = "Preset <?php echo $config; ?> wurde geladen!";
 alert(txt);
 window.top.location.href = 'index.php';
 </script>
-root@pianoledvisualizer:/home/Piano-LED-Visualizer/webinterface/php# cat list_file.php
-<?php
-    $path = $_GET["path"];
-    $file = $_GET["file"];
-    $content="
-        <h4>$path$file</h4>
-            <code>
-                <pre>".htmlspecialchars(file_get_contents("$path/$file"))."</pre>
-            </code>";
-    echo $content;
-?>
