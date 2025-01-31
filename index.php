8eb618381720654c50c32fe65af2d24f2cbeb189
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-01-31 22:03:22 +0100

    Update index.php
    WiFi button added.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="dark" xmlns:x-transition="" lang="eng">
+<html xmlns:x-transition="" lang="eng">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,15 +10,42 @@
   <link rel="mask-icon" href="/imgs/safari-pinned-tab.svg" color="#0ed3cf">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#0ed3cf">
-  <title>Piano LED Visualizer Addon</title>
-  <title>Move Button Example</title>
+  <title>Piano LED Visualizer @-on</title>
   <style>
-    .absolute-button {
+    .button1 {
       position: absolute;
       top: 55px;
       left: calc(50% - 80px);
     }
-    select {
+    @media (min-width: 500px) {
+      .wifi {
+        width: 110px;
+        left: calc(100% - 240px);
+        background: #fff;
+        background: rgba(0,0,0,0) url(./imgs/wifi.png) no-repeat scroll 80px center/18px auto;
+      }
+    }
+    @media (max-width: 500px) {
+      .wifi {
+        width: 36px;
+        left: calc(100% - 164px);
+        background: #fff;
+        background: rgba(0,0,0,0) url(./imgs/wifi.png) no-repeat scroll 8px center/18px auto;
+      }
+    }
+    .wifi {
+      position: absolute;
+      top: 8px;
+      font-size: 12px;
+      border-radius: 10px;
+      padding: 6px 4px;
+      border: 1px solid #d7d7d7;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      color: #000000;
+    }
+    .tools {
       position: absolute;
       top: 8px;
       left: calc(100% - 120px);
@@ -37,6 +64,30 @@
     body {
       background-color: #1c87c9;
     }
+    #message-box {
+    top:2%;
+
+    left:10%;
+    width:80%;
+
+    border: 1px solid black;
+    background-color:#FFFFFF;
+    position:absolute;
+    z-index:10000;
+}
+
+.disabled-body {
+    background-color: rgba(0,0,0,0.5);
+}
+
+.fadeable {
+    opacity: 1;
+    transition: opacity 3s;
+}
+
+.fade {
+    opacity: 0 !important;
+}
   </style>
 </head>
 
@@ -44,9 +95,7 @@
   <script>
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   </script>
-
-  <iframe src="/presets.php" width="100%" height="35" name="PRESETS" title="PLV Presets"frameborder="0"></iframe>
-
+  <iframe src="/presets.php" width="100%" height="36" name="PRESETS" title="PLV Presets"frameborder="0"></iframe>
   <style type=text/css>
     [data-url]::after {
       content: attr(data-url)
@@ -57,25 +106,27 @@
     }
   </style>
   <div> <iframe id="iFrame" src="LED-Piano.php"></iframe> </div>
-  <button class="absolute-button"data-url="LED-Piano.php"type="button">Refresh </button>
-
-    <select id="tools" size="1" name="tools" style="color: white" onchange="window.open(this.value,'location=no','width=800,height=600');selectElement('tools', '');">
-      <optgroup label="System" style="color: black; font-family: 'Times New Roman', Times, serif;">
-        <option selected disabled hidden value="">Tools</option>
-        <option value="rtpMIDI.php">rtpMIDI</option>
-        <option value="./tools.php">System</option>
-      </optgroup>
-      <optgroup label="Other" style="color: black; font-family: 'Times New Roman', Times, serif;">
-        <option value="https://flyingnotes.app">flyingnotes</option>
-        <option value="ledemu2.html">LED Emulator</option>
-      </optgroup>
-      <optgroup label="Info" style="color: black; font-family: 'Times New Roman', Times, serif;">
-        <option value="">About</option>
-        <option value="https://github.com/geg1965/Piano-LED-Visualizer-Addon/">GitHub</option>
-        <option value="https://github.com/geg1965/Piano-LED-Visualizer-Addon/blob/master/README.md">Help</option>
-      </optgroup>
-    </select>
-
+  <button class="button1"data-url="LED-Piano.php"type="button">Refresh </button>
+  <select class="tools" id="tools" size="1" name="tools" style="color: white;" onchange="selectAction(event); selectElement('tools', '');">
+    <optgroup label="System" style="color: black; font-family: 'Times New Roman', Times, serif;">
+      <option selected disabled hidden value="">Tools</option>
+      <option>rtpMIDI</option>
+      <option>System</option>
+    </optgroup>
+    <optgroup label="Other" style="color: black; font-family: 'Times New Roman', Times, serif;">
+      <option>flyingnotes</option>
+      <option>LED Emulator</option>
+    </optgroup>
+    <optgroup label="Info" style="color: black; font-family: 'Times New Roman', Times, serif;">
+      <option>About</option>
+      <option>GitHub</option>
+      <option>Help</option>
+    </optgroup>
+  </select>
+  <select class="wifi" id="wifi" size="1" name="wifi" style="color: black; font-family: 'Times New Roman', Times, serif; text-align: center;" onchange="selectAction(event); selectElement('wifi', '');">
+    <option selected disabled hidden value=""></option>
+    <option>WiFi info</option>
+  </select>
   <script>
     Array.prototype.forEach.call(document.querySelectorAll('[data-url]'), function(elem) {
       elem.addEventListener('click', function(e) {
@@ -85,6 +136,34 @@
     function selectElement(id, valueToSelect) {
       let element = document.getElementById(id);
       element.value = valueToSelect;
+    }
+    function selectAction(evt) {
+      switch (evt.target.value) {
+        case "WiFi info":
+          window.open('./tools/wifi.php','','width=800,height=600');
+          break;
+        case "rtpMIDI":
+          window.open('./tools/rtpMIDI.php', 'tools', 'width=800,height=600');
+          break;
+        case "System":
+          window.open('./tools/system.php', 'tools', 'width=800,height=600');
+          break;
+        case "flyingnotes":
+          window.open('https://flyingnotes.app', 'flyingnotes', 'width=800,height=600');
+          break;
+        case "LED Emulator":
+          window.open('ledemu2.html', 'emulator', 'width=400,height=600');
+          break;
+        case "About":
+          alert('Piano LED Visualizer @-on v1.0');
+          break;
+        case "GitHub":
+          window.open('https://github.com/geg1965/Piano-LED-Visualizer-Addon/');
+          break;
+        case "Help":
+          window.open('https://github.com/geg1965/Piano-LED-Visualizer-Addon/blob/master/README.md');
+          break;
+      }
     }
   </script>
 </body>
