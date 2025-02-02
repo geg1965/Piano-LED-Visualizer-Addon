f552313f23f30af4e70158f6cb134bafd0fddd4b
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-02 19:51:34 +0100

    Update rtpMIDI.php

--- tools/rtpMIDI.php
+++ tools/rtpMIDI.php
@@ -10,9 +10,11 @@
   </head>
 <body>
   <h1 style="color:blue;text-align:center;">Configure static rtpMIDI connections</h1>
-  <h3>After a change of the rtpMIDI configuration, the appropriate input and playback port must be selected in the Piano LED Visualizer on the MIDI page and "CONNECT PORTS" must be executed.</h3><br>
-
-  <form action="set_rtpMIDI.php">
+  <h3>After a change of the rtpMIDI configuration, the appropriate input and playback port must be selected in the Piano LED Visualizer on the MIDI page and "CONNECT PORTS" must be executed.</h3>
+  <iframe src="rtpMIDI_resp.php" width="98%" height="100px" style="border:1px solid black;">
+  </iframe>
+  <h3></h3><br>
+  <form action="rtpMIDI_set.php">
     <label for="IP">IP.........................</label>
     <input type="number" style="width: 4em;" id="ip1" name="ip1" min="0" max="255" value="192" required="required"> .
     <input type="number" style="width: 4em;" id="ip2" name="ip2" min="0" max="255" value="168" required="required"> .
@@ -29,13 +31,14 @@
     <label>Add connection to rtpMIDI configuration....</label>
     <input type="submit" style="color:green;width: 9em;" value="Add connection"><br><br><br>
     <label>Reset rtpMIDI configuration to default........</label>
-    <input type="button" style="color:red;width: 9em;" value="Reset to defaults" onclick="location.href='reset_rtpMIDI.php';">
+    <input type="button" style="color:red;width: 9em;" value="Reset to defaults" onclick="location.href='rtpMIDI_reset.php';">
   </form>
-  <br><br><br>
+  <br><br>
   <h2>The active rtpMIDI configuration:</h2>
   <iframe src="list_file.php?path=/etc/rtpmidid/&file=default.ini" width="98%" height="400px" style="border:1px solid black;">
   </iframe>
-
+  <?php $version = shell_exec("dpkg -l| grep rtp | awk '{print $3}'");?>
+  <font size="1" color="gray">Installed rtpMIDI version: <?= $version ?></font>
   <script>
     function enable_box(status)
     {
