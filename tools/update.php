74ad38be2fef492942b8d17c629c71d4f75cf754
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-03 21:13:09 +0100

    Create update.php

---
+++ tools/update.php
@@ -0,0 +1,8 @@
+<?php
+  shell_exec("git clone https://github.com/geg1965/Piano-LED-Visualizer-Addon ../../php");
+?>
+<script>
+  var txt = "Upadate executed!";
+  alert(txt);
+  window.top.location.href = '/index.php';
+</script>
