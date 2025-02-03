859fc38e7baad47c01dac37f150b5b973a772a3c
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-03 19:29:08 +0100

    Create btMIDI.php

---
+++ tools/btMIDI.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PLV Bluetooth MIDI configuration</title>
+    <style>
+      iframe{
+        background-color:#DDDDDD;
+      }
+    </style>
+  </head>
+<body>
+  <h1 style="color:blue;text-align:center;">Configure Bluetooth connections</h1>
+  <h3 style="text-align:center;">After a change of the Bluetooth MIDI connection, the appropriate input and playback port must be selected in the Piano LED Visualizer on the MIDI page and "CONNECT PORTS" must be executed.</h3><br>
+  <form action="btMIDI_set.php" style="text-align:center;">
+    <label for="mac">MAC Adress:</label>
+    <input type="text" id="mac" name="mac"  placeholder="xx:xx:xx:xx:xx:xx" required="required">
+    <select id="action" required="required" name="action">
+      <option value="connect">Connect</option>
+      <option value="disconnect">Disconnect</option>
+      <option value="pair">Pair</option>
+      <option value="remove">Remove</option>
+      <option value="trust">Trust</option>
+      <option value="untrust">Untrust</option>
+      <option value="block">Block</option>
+      <option value="unblock">Unblock</option>
+    </select>
+    <input type="submit" style="color:green;width: 5em;" value="Submit"><br>
+  </form>
+  <br><br>
+  <h2>Bluetooth devices:</h2>
+  <iframe src="btMIDI_list.php" width="98%" height="400px" style="border:1px solid black;">
+  </iframe>
+  <?php $version = shell_exec("dpkg -l| grep ' bluez ' | awk '{print $3}'");?>
+  <font size="1" color="gray">Installed  bluez version: <?= $version ?></font>
+</body>
+</html>
