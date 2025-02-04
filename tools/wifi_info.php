81f03ff358c2d505c74ecf5f57ad6a314c85cba5
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-04 20:08:11 +0100

    Update wifi_info.php

--- tools/wifi_info.php
+++ tools/wifi_info.php
@@ -37,7 +37,7 @@
     $color2 = "blue";
   }
   ?>
-  <img style="float:left;width:100px;height:38px;" src="/imgs/wifi_info.png">
+  <img style="float:left;width:100px;height:38px;" src="/imgs/wifi.png">
   <img style="float:right;width:100px;height:38px;" src="/imgs/empty.png">
   <h1 style="color:blue;text-align:center;">WLAN Informations</h1>
   <font size="1" color="<?php echo $color1; ?>">WiFi signal -<?php echo $wlan; ?>dBm</font>
