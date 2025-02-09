5ea7add0fb02ed30df2e6ec32564c487c62a1bed
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-09 21:31:11 +0100

    Update system.php
    Form to set MIDI program change channel.

--- tools/system.php
+++ tools/system.php
@@ -14,11 +14,36 @@
   <img style="float:right;width:100px;height:38px;" src="/imgs/empty.png">
   <h1 style="color:blue;text-align:center;">Raspberry PI OS tools and infos</h1>
   <form style="text-align:center;" action="set_pw.php">
-    <label for="port">Enter new password for user "plv" </label>
+    <label for="password">Enter new password for user "plv" </label>
     <input type="password" id="password" name="password" placeholder="empty for default">
     <input type="submit" style="color:red;width: 9em;" value="set password">
     <input type="checkbox" onclick="myFunction()">Show Password
+  </form><br>
+
+  <form style="text-align:center;" method="post" action="">
+    <?php
+      $selected = shell_exec("cat ./inis/addon.ini") + 1;
+    ?>
+    <label for="channel">Global MIDI-Channel for PLV preset change </label>
+    <select method="post" name="channel" onchange="this.form.submit()">
+      <optgroup label="Select" style="color: gray; font-family: 'Times New Roman', Times, serif;">
+      <option disabled hidden value="">Channel</option>
+      <option <?php if($selected == 99){echo("selected");}?> style="color: black" value="99">Off</option>
+      <?php
+        for ($i = 1; $i <= 16; $i++) {
+          echo "<option "; if($selected == $i){echo("selected ");}; echo "style=\"color: black\" value=\"$i\">Ch. $i</option>";
+        }
+      ?>
+    </select>
   </form>
+  <?php
+    if(isset($_POST["channel"])) {
+      $channel=$_POST["channel"] - 1;
+      shell_exec("echo $channel > ./inis/addon.ini");
+      header("Refresh:0");
+    }
+  ?>
+
   <br>
   <h2>OS Information:</h2>
   <iframe src="list_file.php?path=/etc/&file=os-release" width="98%" height="220px" style="border:1px solid black;">
