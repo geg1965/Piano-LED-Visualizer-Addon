9b229e376ef211948eaf5bb39e0a8d865f7a2ffa
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 00:58:58 +0100

    Create download.php

---
+++ tools/download.php
@@ -0,0 +1,29 @@
+<?php
+  $file = $_GET["file"];
+  $preset = $_GET['preset'];
+  $txt = "Preset not found!";
+
+  if ($preset == "all") {
+    $file = "$file.plv_bank";
+    shell_exec("zip ./downloads/$file ../cfgs/*.xml.* -x *.default");
+  } else {
+    $file = "$file.plv_preset";
+    shell_exec("zip ./downloads/$file ../cfgs/*.xml.$preset");
+    $txt = "PLV-Preset $preset was downloaded.";
+  }
+  if(!file_exists("./downloads/$file")){ // file does not exist
+    echo '<script>alert("Preset not available")</script>';
+    echo '<script>history.back()</script>';
+  } else {
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$file");
+    header("Content-Type: application/zip");
+    header("Content-Transfer-Encoding: binary");
+
+    // read the file from disk
+    readfile("./downloads/$file");
+    unlink("./downloads/$file");
+  }
+
+?>
