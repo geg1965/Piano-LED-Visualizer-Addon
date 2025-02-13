34e741f94052d7d3dc7e9bd2daf06187d73de3f4
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 21:32:40 +0100

    Update update.php

--- tools/update.php
+++ tools/update.php
@@ -2,9 +2,8 @@
   shell_exec("git clone https://github.com/geg1965/Piano-LED-Visualizer-Addon update");
   shell_exec("rsync -r ./update/* ../../php; rm -Rf update");
   shell_exec("chmod +x ./post_update; ./post_update");
+  echo ("<script>");
+  echo ("alert(\"Update completed!\");");
+  echo ("window.top.location.href = '/index.php';");
+  echo ("</script>");
 ?>
-<script>
-  var txt = "Update completed!";
-  alert(txt);
-  window.top.location.href = '/index.php';
-</script>
