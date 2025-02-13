046e7939670a3af78f0288a316b96bc4907aa261
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 14:46:26 +0100

    Update upload.php

--- tools/upload.php
+++ tools/upload.php
@@ -1,48 +1,56 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-echo "$target_file";
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 1;
+  $preset = $_POST["preset"];
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  $tmp_file = $_FILES["fileToUpload"]["tmp_name"];
+  if(isset($_POST["submit"])) {
+    $check = shell_exec("unzip -l '$tmp_file' | grep -c settings.xml");
+    if($check == 0) {
+      echo '<script>alert("File is not a valid plv file.")</script>';
+      $uploadOk = 0;
+    }
+    if($FileType == "plv_preset" && $check > 1) {
+      echo '<script>alert("File is not a valid plv_preset file.")</script>';
+      $uploadOk = 0;
+    }
   }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($FileType != "plv_preset" && $FileType != "plv_bank" ) {
-  echo "Sorry, only plv_preset & plv_bank files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
+  if (file_exists($target_file)) {
+    echo '<script>alert("Sorry, file already exists.")</script>';
+    $uploadOk = 0;
   }
-}
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo '<script>alert("Sorry, your file is too large.")</script>';
+    $uploadOk = 0;
+  }
+  if($FileType != "plv_preset" && $FileType != "plv_bank") {
+    echo '<script>alert("Sorry, only plv_preset & plv_bank files are allowed.")</script>';
+    $uploadOk = 0;
+  }
+  if ($uploadOk == 1) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      if($FileType == "plv_bank" && "$preset" == "all"){
+        shell_exec("find ../cfgs -type f -name '*.xml.*' ! -name '*.default' -delete");
+        shell_exec("unzip -d .. '$target_file'");
+        echo '<script>alert("The bank file has been stored.")</script>';
+      } elseif ($FileType == "plv_bank") {
+        echo '<script>alert("Please select \"Bank\" to restore a bank file!")</script>';
+      }
+      if($FileType == "plv_preset" && ("$preset" == "all" | "$preset" == "")){
+        echo '<script>alert("Please select a preset to store a preset file!")</script>';
+      } elseif ($FileType == "plv_preset") {
+        shell_exec("unzip -d ./uploads '$target_file'");
+        shell_exec("mv ./uploads/cfgs/settings.xml.* ../cfgs/settings.xml.$preset");
+        shell_exec("mv ./uploads/cfgs/sequences.xml.* ../cfgs/sequences.xml.$preset");
+        shell_exec("rm -Rf ./uploads/cfgs/");
+        echo '<script>alert("The preset file has been stored.")</script>';
+      }
+//      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+      echo '<script>alert("Sorry, there was an error uploading your file.")</script>';
+    }
+    unlink("$target_file");
+  }
+  echo '<script>history.back()</script>';
 ?>
