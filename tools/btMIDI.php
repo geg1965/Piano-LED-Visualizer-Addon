13211df21028bcdda6d2128671584ecef5227a19
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 20:45:08 +0100

    Update btMIDI.php

--- tools/btMIDI.php
+++ tools/btMIDI.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>btMIDI setup</title>
 <html>
   <head>
     <title>PLV Bluetooth MIDI configuration</title>
