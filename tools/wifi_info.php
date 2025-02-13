08fafcd96643658b994183394e2d8584b8bc503e
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 23:59:30 +0100

    Update wifi_info.php

--- tools/wifi_info.php
+++ tools/wifi_info.php
@@ -1,6 +1,8 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>WiFi Informations</title>
   <script type="text/javascript">
     function refreshPopup() {
