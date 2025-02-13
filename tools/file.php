118a64d6111f6bf23340a7d3561e511068befb6e
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 14:48:08 +0100

    Update file.php

--- tools/file.php
+++ tools/file.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Preset Backup & Restore</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Preset Backup & Restore</title>
 </head>
 <body>
   <img style="float:left;width:100px;height:38px;" src="/imgs/file.png">
@@ -12,14 +12,25 @@
   <h3 style="text-align:center;">Individual presets and preset banks can be saved and restored. When restoring individual presets, the destination can be selected. Restoring an entire bank will overwrite or delete existing presets.</h3><br>
 
   <form action="upload.php" method="post" enctype="multipart/form-data" style="text-align:center;">
-    Select preset- or bank-file to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload" accept=".plv_bank,.plv_preset">
+    Select file:
+    <input type="file" name="fileToUpload" id="fileToUpload" accept=".plv_bank,.plv_preset" required="required">
+    <select id="preset" size="1" name="preset">
+      <optgroup label="Destination:" style="color: gray; font-family: 'Times New Roman', Times, serif;">
+      <option selected disabled hidden value="">Select Preset</option>
+      <option style="color: black" value="all">Bank</option>
+      <?php
+        for ($i = 1; $i <= 128; $i++) {
+          echo "<option style=\"color: black\" value=\"$i\">Preset $i</option>";
+        }
+      ?>
+      </optgroup>
+    </select>
     <input type="submit" value="Upload" name="submit">
   </form><br><br>
 
   <form action="download.php" style="text-align:center;">
-    Set name for download-file
-    <input type="test" name="file" id="fileToDownload" required="required">
+    Download file name:
+    <input type="test" name="file" id="fileToDownload" required="required" pattern="[A-Za-z0-9²³@€\-\(\)\{\}\[\]_ #+&%$§!]{1,25}">
     <select id="preset" size="1" name="preset">
       <optgroup label="Save Preset:" style="color: gray; font-family: 'Times New Roman', Times, serif;">
       <option selected disabled hidden value="">Select Preset</option>
@@ -33,5 +44,18 @@
     </select>
     <input type="submit" value="Download" name="submit">
   </form><br><br>
+  <div style="text-align:center">
+    <button  style="color:red;" onclick="delete_presets()">Reset all Presets</button>
+  </div>
+  <script>
+    function delete_presets() {
+     if (confirm('Are you sure you want to delete all presets?')) {
+       <?php shell_exec("find ../cfgs -type f -name '*.xml.*' ! -name '*.default' -delete"); ?>;
+       alert("All presets where deleted!");
+       var url= "/tools/file.php";
+       window.location = url;
+     }
+    }
+  </script>
 </body>
 </html>
