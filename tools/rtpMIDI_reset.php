31d932189a860a9aa0100c4472cb10cae7962940
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-13 21:03:15 +0100

    Update rtpMIDI_reset.php

--- tools/rtpMIDI_reset.php
+++ tools/rtpMIDI_reset.php
@@ -2,8 +2,8 @@
   shell_exec("cp ./inis/default.ini /etc/rtpmidid/");
   shell_exec("systemctl restart rtpmidid");
   sleep(1);
+  echo ("<script>");
+  echo ("alert(\"The rtpMIDI configuration was set to default!\");");
+  echo ("window.top.location.href = 'rtpMIDI.php';");
+  echo ("</script>");
 ?>
-<script>
-  alert("The rtpMIDI configuration was set to default!");
-  window.top.location.href = 'rtpMIDI.php';
-</script>
