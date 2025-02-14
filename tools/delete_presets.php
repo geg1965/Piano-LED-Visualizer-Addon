f3fd139091dfc9f6d2f4d8b8165dd6f1983b5a8b
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-02-14 14:03:18 +0100

    Update delete_presets.php

--- tools/delete_presets.php
+++ tools/delete_presets.php
@@ -1,7 +1,17 @@
 <?php
-  shell_exec("find ../cfgs -type f -name '*.xml.*' ! -name '*.default' -delete");
-  echo("<script>");
-  echo("alert('All presets where deleted!');");
-  echo("window.top.location.href = 'file.php';");
-  echo("</script>");
+  $preset = $_GET["preset"];
+  $txt = "";
+  if ($preset == "") {
+    $txt = "Select the preset you want to delete.";
+  } elseif ($preset == "all") {
+    shell_exec("find ../cfgs -type f -name '*.xml.*' ! -name '*.default' -delete");
+    $txt = "All presets deleted!";
+  } else {
+    shell_exec("rm ../cfgs/*.xml.$preset");
+    $txt = "Preset $preset was deleted!";
+  }
+  echo ("<script>");
+  echo ("alert(\"$txt\");");
+  echo ("window.top.location.href = 'file.php';");
+  echo ("</script>");
 ?>
