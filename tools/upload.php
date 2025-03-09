d9ba252e95094d793c2047d9b1ab524c786bae93
geg1965 <82303353+geg1965@users.noreply.github.com>
2025-03-09 16:45:12 +0100

    Update upload.php
    Support for MIDI file bank backup and restore

--- tools/upload.php
+++ tools/upload.php
@@ -6,7 +6,11 @@
   $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
   $tmp_file = $_FILES["fileToUpload"]["tmp_name"];
   if(isset($_POST["submit"])) {
-    $check = shell_exec("unzip -l '$tmp_file' | grep -c settings.xml");
+    if($FileType == "plv_midi") {
+      $check = shell_exec("unzip -l '$tmp_file' | grep -c .mid");
+    } else {
+      $check = shell_exec("unzip -l '$tmp_file' | grep -c settings.xml");
+    }
     if($check == 0) {
       echo '<script>alert("File is not a valid plv file.")</script>';
       $uploadOk = 0;
@@ -24,8 +28,8 @@
     echo '<script>alert("Sorry, your file is too large.")</script>';
     $uploadOk = 0;
   }
-  if($FileType != "plv_preset" && $FileType != "plv_bank") {
-    echo '<script>alert("Sorry, only plv_preset & plv_bank files are allowed.")</script>';
+  if($FileType != "plv_preset" && $FileType != "plv_bank" && $FileType != "plv_midi") {
+    echo '<script>alert("Sorry, only plv_preset, plv_bank & plv_midi files are allowed.")</script>';
     $uploadOk = 0;
   }
   if ($uploadOk == 1) {
@@ -35,10 +39,10 @@
         shell_exec("unzip -d .. '$target_file'");
         echo '<script>alert("The bank file has been stored.")</script>';
       } elseif ($FileType == "plv_bank") {
-        echo '<script>alert("Please select \"Bank\" to restore a bank file!")</script>';
+        echo '<script>alert("Please select \"Preset Bank\" to restore a bank file!")</script>';
         echo '<script>history.back();</script>';
       }
-      if($FileType == "plv_preset" && ("$preset" == "all" | "$preset" == "")){
+      if($FileType == "plv_preset" && ("$preset" == "all" | "$preset" == "" | "$preset" == "midi")){
         echo '<script>alert("Please select a preset to store a preset file!")</script>';
         echo '<script>history.back();</script>';
       } elseif ($FileType == "plv_preset") {
@@ -47,6 +51,13 @@
         shell_exec("mv ./uploads/cfgs/sequences.xml.* ../cfgs/sequences.xml.$preset");
         shell_exec("rm -Rf ./uploads/cfgs/");
         echo '<script>alert("The preset file has been stored.")</script>';
+      }
+      if($FileType == "plv_midi" && ("$preset" != "midi" | "$preset" == "")){
+        echo '<script>alert("Please select \"MIDI Bank\" to restore MIDI files!")</script>';
+        echo '<script>history.back();</script>';
+      } elseif ($FileType == "plv_midi") {
+        shell_exec("rm -r ../../../Songs/*; unzip -d ../../.. '$target_file'");
+        echo '<script>alert("The MIDI files have been stored.")</script>';
       }
     } else {
       echo '<script>alert("Sorry, there was an error uploading your file.")</script>';
